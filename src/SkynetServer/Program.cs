45731dd7ee9711898dcec2c95f6171aadeab33a2
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-10-31 16:00:57 +0100

    Create solution

---
+++ src/SkynetServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkynetServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
