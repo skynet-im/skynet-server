2ab280668b95157f0a100c0bf85b97505796ccaf
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-12-22 18:43:54 +0100

    Structure for unencrypted channel messages

---
+++ src/SkynetServer/Packets/ChannelMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkynetServer.Packets
+{
+    internal abstract class ChannelMessage : Packet
+    {
+        public long ChannelId { get; set; }
+        public long SenderId { get; set; }
+        public long MessageId { get; set; }
+        public long SkipCount { get; set; }
+        public DateTime DispatchTime { get; set; }
+        public MessageFlags MessageFlags { get; set; }
+        public long FileId { get; set; }
+        public List<MessageDependency> Dependencies { get; set; } = new List<MessageDependency>();
+    }
+}
