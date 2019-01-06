79debd2888b36c267d76732566526865b974469d
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-01-07 00:29:31 +0100

    Move from Razor Pages to MVC

---
+++ src/SkynetServer.Web/Controllers/VerificationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkynetServer.Web.Controllers
+{
+    [Route("~/verification")]
+    public class VerificationController : Controller
+    {
+        [HttpGet("{token}")]
+        public IActionResult Get(string token)
+        {
+            return View("Pending");
+        }
+
+        [HttpPost("{token}")]
+        public IActionResult Post(string token)
+        {
+            return View("Success");
+        }
+    }
+}
