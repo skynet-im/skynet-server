6f4c3af87c8862f4b4a552b0e71f207f9169729f
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-01-09 11:36:22 +0100

    Add SkynetServer.Cli project

---
+++ src/SkynetServer.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkynetServer.Cli
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
