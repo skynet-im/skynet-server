4f2ce7ce6a8c325855774f339be3d25bc1ff2760
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-02-05 15:53:44 +0100

    Add unit tests for database helper methods

---
+++ tests/SkynetServer.Database.Tests/DatabaseHelperTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkynetServer.Entities;
+using SkynetServer.Model;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Wiry.Base32;
+
+namespace SkynetServer.Database.Tests
+{
+    [TestClass]
+    public class DatabaseHelperTests
+    {
+        [TestMethod]
+        public void TestAddAccount()
+        {
+            Parallel.For(0, 500, i =>
+            {
+                Account account = new Account() { AccountName = $"{RandomAddress()}@example.com", KeyHash = new byte[0] };
+                DatabaseHelper.AddAccount(account);
+            });
+        }
+
+        [TestMethod]
+        public void TestAddAccountAndSession()
+        {
+            Parallel.For(0, 100, i =>
+            {
+                Account account = new Account() { AccountName = $"{RandomAddress()}@example.com", KeyHash = new byte[0] };
+                DatabaseHelper.AddAccount(account);
+
+                Parallel.For(0, 10, j =>
+                {
+                    Session session = new Session()
+                    {
+                        AccountId = account.AccountId,
+                        ApplicationIdentifier = "windows/SkynetServer.Database.Tests",
+                        CreationTime = DateTime.Now
+                    };
+                    DatabaseHelper.AddSession(session);
+                });
+            });
+        }
+
+        [TestMethod]
+        public void TestAddChannel()
+        {
+            Parallel.For(0, 500, i =>
+            {
+                Channel channel = new Channel() { ChannelType = ChannelType.Loopback };
+                DatabaseHelper.AddChannel(channel);
+            });
+        }
+
+        [TestMethod]
+        public void TestAddChannelAndMessage()
+        {
+            Parallel.For(0, 50, i =>
+            {
+                Channel channel = new Channel() { ChannelType = ChannelType.Loopback };
+                DatabaseHelper.AddChannel(channel);
+
+                Parallel.For(0, 500, j =>
+                {
+                    Message message = new Message() { ChannelId = channel.ChannelId, DispatchTime = DateTime.Now };
+                    DatabaseHelper.AddMessage(message);
+                });
+            });
+        }
+
+        private string RandomAddress()
+        {
+            using (var random = RandomNumberGenerator.Create())
+            {
+                byte[] value = new byte[10];
+                random.GetBytes(value);
+                return Base32Encoding.Standard.GetString(value).ToLower();
+            }
+        }
+    }
+}
