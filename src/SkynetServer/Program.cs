7134a0c5d3e91d415846f7574a7690ff1085f25c
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-02-19 17:19:24 +0100

    Fix configuration and docker compose

--- src/SkynetServer/Program.cs
+++ src/SkynetServer/Program.cs
@@ -20,6 +20,14 @@ namespace SkynetServer
 
         private static Task Main(string[] args)
         {
+            // The appsettings.json file contained in this repository lacks some secrets that are necessary for production usage.
+            // Our debug keypair "<Modulus>jKoWxmIf..." should be used in all client applications to connect to development servers.
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Clients = ImmutableList.Create<Client>();
+
             return CreateHostBuilder(args).Build().RunAsync();
         }
 
@@ -28,9 +36,7 @@ namespace SkynetServer
             HostBuilder builder = new HostBuilder();
             builder.ConfigureAppConfiguration(config =>
             {
-                // The appsettings.json file contained in this repository lacks some secrets that are necessary for production usage.
-                // Our debug keypair "<Modulus>jKoWxmIf..." should be used in all client applications to connect to development servers.
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddConfiguration(Configuration);
             });
             builder.ConfigureServices(services =>
             {
