3d544891f7c861d8fb046ab4e7cb5aec3554262b
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-11-28 19:24:51 +0530

    Implement core components for TLS usage

---
+++ src/SkynetServer/Sockets/PacketStream.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SkynetServer.Sockets
+{
+    internal class PacketStream
+    {
+        private readonly Stream stream;
+
+        public PacketStream(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public async ValueTask<(byte id, ReadOnlyMemory<byte> buffer)> ReadAsync()
+        {
+            byte[] buffer = new byte[4];
+            await ReadInternal(buffer);
+            int packetMeta = BitConverter.ToInt32(buffer);
+            if (!BitConverter.IsLittleEndian)
+                packetMeta = BinaryPrimitives.ReverseEndianness(packetMeta);
+
+            byte id = (byte)(packetMeta & 0x000000FF);
+            int length = packetMeta >> 8;
+
+            buffer = new byte[length];
+            await ReadInternal(buffer);
+
+            return (id, buffer);
+        }
+
+        public async ValueTask WriteAsync(byte id, ReadOnlyMemory<byte> buffer)
+        {
+            if (buffer.Length > 0x00ffffff) throw new ArgumentOutOfRangeException(nameof(buffer));
+
+            int packetMeta = buffer.Length << 8 | id;
+            if (!BitConverter.IsLittleEndian)
+                packetMeta = BinaryPrimitives.ReverseEndianness(packetMeta);
+
+            await stream.WriteAsync(BitConverter.GetBytes(packetMeta));
+            await stream.WriteAsync(buffer);
+        }
+
+        private async ValueTask ReadInternal(Memory<byte> buffer)
+        {
+            int read = 0;
+            do
+            {
+                read += await stream.ReadAsync(buffer.Slice(read));
+            } while (read < buffer.Length);
+        }
+    }
+}
---
+++ src/SkynetServer/Sockets/SslListener.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkynetServer.Sockets
+{
+    internal sealed class SslListener : IDisposable
+    {
+        private readonly Socket listener;
+        private readonly X509Certificate certificate;
+
+        public SslListener(int port, X509Certificate certificate)
+        {
+            this.certificate = certificate ?? throw new ArgumentNullException(nameof(certificate));
+
+            listener = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp)
+            {
+                DualMode = true
+            };
+
+            try
+            {
+                listener.Bind(new IPEndPoint(IPAddress.IPv6Any, port));
+                listener.Listen(64);
+            }
+            catch (SocketException)
+            {
+                listener.Dispose();
+            }
+        }
+
+        public async Task<PacketStream> AcceptAsync()
+        {
+            if (disposedValue) throw new ObjectDisposedException(nameof(SslListener));
+
+            // TODO: Handle execeptions to prevent memory leaks
+            Socket client = await listener.AcceptAsync().ConfigureAwait(false);
+            SslStream sslStream = new SslStream(new NetworkStream(client), leaveInnerStreamOpen: false);
+            await sslStream.AuthenticateAsServerAsync(certificate, false, SslProtocols.Tls13, false).ConfigureAwait(false);
+            return new PacketStream(sslStream);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    listener.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // ~SslListener()
+        // {
+        //   Dispose(false);
+        // }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
